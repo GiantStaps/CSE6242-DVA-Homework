7f8833bab6f0b3af2bdfae882283e81e984d9eb3
Ma <rma86@gatech.edu>
2024-10-02 13:09:12 -0400

    Done Q5

--- HW2/Q4/Q4.js
+++ HW2/Q4/Q4.js
@@ -1,4 +1,5 @@
 // Function to read data and return a Promise that resolves with the data and maxRating
+let colorScale = d3.scaleOrdinal(d3.schemeCategory10); // A color scale with distinct colors
 function loadData(pathToCsv) {
     let maxRating = 0; // Initialize maxRating
 
@@ -19,32 +20,53 @@ function loadData(pathToCsv) {
     });
 }
 
-// Line Chart function
-function createLineChart(data, maxRating, yearInterested, margin, width, height) {
+// Function that does analysis on the data
+function dataProcessing(data, maxRating, yearInterested) {
 
     let yearDict = {};
     let maxSumCounts = 0;
-    const rangeArray = Array.from({ length: maxRating + 1 }, (_, i) => i);
+    const rantingRange = Array.from({ length: maxRating + 1 }, (_, i) => i);
+
     yearInterested.forEach(year => {
         // Filter data for the current year
         const filteredYearData = data.filter(d => d.year === year);
-    
+        let sums = [];
+        let topFiveGames = [];
+
         // Sum the counts for ratings within the range
-        const sumCountsByRating = rangeArray.map(rating => {
-            const sum = filteredYearData
-                .filter(d => d.rating === rating)   // Filter by the specific rating
-                .reduce((sum, d) => sum + d.count, 0);  // Sum up the `count` for that rating
-            
-            maxSumCounts = Math.max(maxSumCounts, sum);    
-            return sum;
-        });
+        // While also getting top 5 games for each rating
+        rantingRange.forEach(rating => {
+
+            const filteredYearRatingData = filteredYearData
+                                            .filter(d => d.rating === rating)
+            const sum = filteredYearRatingData
+                                .reduce((sum, d) => sum + d.count, 0)
+
+            sums.push(sum);
+
+            maxSumCounts = Math.max(maxSumCounts, sum);
+
+            topFiveGames.push(filteredYearRatingData
+                            .sort((v1, v2) => d3.descending(v1.count, v2.count))
+                            .slice(0, 5)
+                            .map(d => ({name: d.name, count: d.count}))
+                            .reverse()
+                        );
+        })
+
     
         // Set the object for the current year in yearDict
         yearDict[year] = {
-            sumCounts: sumCountsByRating,
+            sumCounts: sums,
+            topFiveGames: topFiveGames,
             details: filteredYearData
         };
     });
+    return {yearDict, maxSumCounts};
+}
+
+// Line Chart function
+function createLineChart(yearDict, maxRating, maxSumCounts, yearInterested, margin, width, height) {
 
     // Create scales
     const x = d3.scaleLinear()
@@ -61,14 +83,15 @@ function createLineChart(data, maxRating, yearInterested, margin, width, height)
     // Create a function to add the y-axis
     const yAxis = g => g
         .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y));
+        .call(d3.axisLeft(y)
+        .tickFormat(d => d / 1000));
 
     const xAxisLabel = g => g
         .append("text")
         .attr("x", (width - margin.left - margin.right) / 2 + margin.left)
         .attr("y", height - margin.bottom + 40)
         .attr("class", "label")
-        .text("Month");
+        .text("Rating");
 
     const yAxisLabel = g => g
         .append("text")
@@ -76,19 +99,17 @@ function createLineChart(data, maxRating, yearInterested, margin, width, height)
         .attr("y", margin.left - 60)
         .attr("class", "label")
         .attr("transform", "rotate(-90)")
-        .text("Num of Ratings");
+        .text("Count");
     
     let line = d3.line()
         .x((d, i) => x(i))
         .y(d => y(d));
 
-    let colorScale = d3.scaleOrdinal(d3.schemeCategory10); // A color scale with distinct colors
-
     const plot_lines_and_legends = (lines_g, circles_g, legend_g) => {
         
         // for each year
         yearInterested.forEach((year, index) => {
-            
+
             // plot the sumCounts
             lines_g.append("path")
                 .datum(yearDict[year].sumCounts)
@@ -102,8 +123,40 @@ function createLineChart(data, maxRating, yearInterested, margin, width, height)
                 circles_g.append("circle")
                     .attr("cx", x(i)) // Set x position using your x scale
                     .attr("cy", y(d)) // Set y position using your y scale
-                    .attr("r", 3) // Set the radius of the circle
-                    .attr("fill", colorScale(index)); // Assign a color based on the index, matching the line color
+                    .attr("r", 6) // Set the radius of the circle
+                    .attr("fill", colorScale(index)) // Assign a color based on the index, matching the line color
+                    .on("mouseover", function(event) {
+                        // Update yearRatingData based on the year and index
+                        
+                        yearRatingData = yearDict[year].topFiveGames[i];
+                        if (yearRatingData.length !== 0) {
+                            // Create a new title and bar chart with the updated data
+
+                            createBarChartTitle(year, i, margin, width, height);
+                            createBarChart(index, yearRatingData, margin, width, height);
+                            
+                            d3.select("#bar_chart_title").style("display", "block");
+                            d3.select("#bar_chart").style("display", "block");
+                        }
+                    
+                        // Optionally, highlight the circle
+                        d3.select(this).attr("r", 12); // Increase circle size on hover
+                    })
+                    .on("mouseout", function() {
+
+                        let bars = d3.select("#bar_chart")
+                        .selectAll(".bar")
+                        .data([]);
+                
+                        bars.exit().remove(); // Remove all bars since there is no data bound to them
+                
+                        // Remove the existing bar chart when mouse moves out
+                        d3.select("#bar_chart_title").style("display", "none");
+                        d3.select("#bar_chart").style("display", "none");
+                    
+                        // Reset the circle to its original size
+                        d3.select(this).attr("r", 6);
+                    });
             });
 
             legend_g.append("circle")
@@ -126,6 +179,7 @@ function createLineChart(data, maxRating, yearInterested, margin, width, height)
                 .attr("x", width / 2.3 )
                 .attr("y", 0)
                 .attr("class", "title")
+                .attr("id", "line_chart_title")
                 .text("Board games by Rating 2015-2019");
 
     const credit = g => g
@@ -133,10 +187,8 @@ function createLineChart(data, maxRating, yearInterested, margin, width, height)
                 .attr("x", width / 2.3)
                 .attr("y", 20)
                 .attr("class", "rank")
+                .attr("id", "credit")
                 .text("rma86");
-
-    const legend = g => g
-                .append
 
     function plotChart() {
 
@@ -173,9 +225,9 @@ function createLineChart(data, maxRating, yearInterested, margin, width, height)
         container.call(chart_title)
                 .call(credit);
         
-        let legend = svg.append("g")
+        let legend = container.append("g")
             .attr("id", "legend")
-            .attr("transform", `translate(${width}, ${margin.top + 50})`); // Position legend above the chart
+            .attr("transform", `translate(${width-margin.right/1.5}, ${margin.top})`); // Position legend above the chart
         
         container.call(xAxisLabel)
                 .call(yAxisLabel);
@@ -183,29 +235,163 @@ function createLineChart(data, maxRating, yearInterested, margin, width, height)
         plot_lines_and_legends(lines, circles, legend);
     }
     plotChart();
+    chart_ready = true;
+}
 
-    return {
+function createBarChartTitle(curr_year, curr_rating, margin, width, height) {
+    let title = d3.select("#bar_chart_title")
+    if (title.empty()) {
+        d3.select("body").append("div")
+            .attr("class", "title")
+            .attr("id", "bar_chart_title")
+            .style("left", (width/2.3 - margin.left) + "px") // Position it based on margin and chart width
+            .style("top", margin.top + "px") // Position it based on top margin
+            .style("display", "none")
+            .text(`Top 5 Most Rated Games of ${curr_year} with Rating ${curr_rating}`);
+    } else {
+        title.text(`Top 5 Most Rated Games of ${curr_year} with Rating ${curr_rating}`);
+    }
+}
 
+function createBarChart(curr_year, yearRatingData, margin, width, height) {
+
+    // Create scales
+    var x = d3.scaleLinear()
+                .domain([0, d3.max(yearRatingData, d => d.count)])
+                .range([margin.left, width - margin.right]);
+
+    var y = d3.scaleBand()
+                .domain(yearRatingData.map(d => d.name))
+                .range([height - margin.bottom, margin.top])
+                .padding(0.2);
+
+    // Add bars to the chart
+    const addBars = function(g) {
+        g.selectAll("rect")
+            .data(yearRatingData)
+            .enter()
+            .append("rect")
+            .attr("x", x(0)) // Start each bar at x = 0 (aligned to the zero of the x-axis)
+            .attr("y", d => y(d.name)) // Position the bar based on the categorical y scale
+            .attr("width", d => x(d.count) - x(0)) // Set the width of each bar based on the count value
+            .attr("height", y.bandwidth()) // Use the bandwidth of the y scale for the height of the bar
+            .attr("fill", colorScale(curr_year));
     };
+
+    const xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x)
+                .tickSize(-height + margin.top + margin.bottom));
+    
+    // Create a function to add the y-axis
+    const yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y)
+                .tickFormat(d => d.length > 10 ? d.slice(0, 10) : d)
+            );
+
+    const xAxisLabel = g => g
+                .append("text")
+                .attr("x", (width - margin.left - margin.right) / 2 + margin.left)
+                .attr("y", height - margin.bottom + 40)
+                .attr("class", "label")
+                .attr("id", "bar_x_axis_label")
+                .text("Number of Users");
+
+    const yAxisLabel = g => g
+                .append("text")
+                .attr("x", -(height / 2))
+                .attr("y", margin.left - 60)
+                .attr("class", "label")
+                .attr("transform", "rotate(-90)")
+                .attr("id", "bar_y_axis_label")
+                .text("Gmaes");
+
+    const plotChart = () => {
+        
+        let svg = d3.select("#bar_chart");
+
+        if (svg.empty()) {
+            svg = d3.select("body")
+                .append("svg")
+                .attr("id", "bar_chart")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .style("display", "none");
+
+            let container = svg.append("g")
+                .attr("id", "container_2")
+                .attr("transform", `translate(${margin.left}, ${margin.top})`);
+            
+            let bars = container.append("g")
+                                .attr("id", "bars")
+                                .call(addBars);
+            
+            // Append the group for x-axis
+            let xAxisBars = container.append("g")
+                .attr("id", "x-axis-bars")
+                .call(xAxis);
+
+            // Append the group for y-axis
+            let yAxisBars = container.append("g")
+                .attr("id", "y-axis-bars")
+                .call(yAxis);
+            
+            container
+                .call(xAxisLabel);
+            container
+                .call(yAxisLabel);
+        } else {
+            // just do the update
+            x.domain([0, d3.max(yearRatingData, d => d.count)]);
+            y.domain(yearRatingData.map(d => d.name));
+        
+            // Update axes with new scales
+            svg.select("#x-axis-bars").call(xAxis);
+            svg.select("#y-axis-bars").call(yAxis);
+        
+            // Bind data to bars
+            let bars = svg.select("#bars").selectAll("rect").data(yearRatingData);
+
+            // Enter new bars if necessary
+            bars.enter()
+                .append("rect")
+                .attr("class", "bar")
+                .merge(bars)
+                .attr("x", margin.left)
+                .attr("y", d => y(d.name))
+                .attr("width", d => x(d.count) - margin.left)
+                .attr("height", y.bandwidth())
+                .attr("fill", colorScale(curr_year));
+        }
+
+    }
+    plotChart();
 }
 
-function createBarChart(data) {
-    
-}
 
 async function main() {
+
+    var yearInterested = [2015, 2016, 2017, 2018, 2019];
     // Load data and maxRating first
     const pathToCsv = "./average-rating.csv";
     const { data, maxRating } = await loadData(pathToCsv);
+    const { yearDict, maxSumCounts } = dataProcessing(data, maxRating, yearInterested);
 
     // Define parameters for the chart
-    const yearInterested = [2015, 2016, 2017, 2018, 2019];
-    const margin = { top: 50, right: 150, bottom: 60, left: 80 };
-    const width = 900;
-    const height = 600;
-
-    // Now call createLineChart with loaded data
-    createLineChart(data, maxRating, yearInterested, margin, width, height);
+    var margin = { top: 50, right: 150, bottom: 60, left: 80 };
+    var width = 900;
+    var height = 600;
+    var curr_year = 2019,
+        curr_rating = 6;
+    createLineChart(yearDict, maxRating, maxSumCounts, yearInterested, margin, width, height)    
+    createBarChartTitle(curr_year, curr_rating, margin, width, height);
+    yearRatingData = yearDict[curr_year].topFiveGames[curr_rating];
+    createBarChart(curr_year, yearRatingData, margin, width, height);
+    return yearDict;
 }
 
-main();
+let yearDict;
+main().then(result => {
+    yearDict = result;
+});
